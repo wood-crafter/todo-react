a7454bac74ed4d207aa073ed9bcea151b64196a7
hungpv <phanhung2702@gmail.com>
2022-04-15 09:50:09 +0700

    [Delete] remove TodoDetail

--- src/detail/Detail.js
+++ src/detail/Detail.js
@@ -1,14 +1,12 @@
 import '../App.css';
 import React from "react";
 import Schedule from './Schedule';
-import TodoDetail from './TodoDetail';
 
 export default class Detail extends React.Component {
   render() {
     return (
       <div className="Detail">
         <Schedule />
-        <TodoDetail />
       </div>
     )
   }
