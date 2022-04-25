35594909f4ae5416382c6d26e65c0cdee1afccc3
hungpv <phanhung2702@gmail.com>
2022-04-25 16:34:13 +0700

    [Add] todo-detail

--- src/detail/Schedule.js
+++ src/detail/Schedule.js
@@ -7,22 +7,27 @@ export default class Schedule extends React.Component {
       '12 PM', '1 PM', '2 PM', '3 PM', '4 PM', '5 PM', '6 PM', '7 PM', '8 PM', '9 PM', '10 PM', '11 PM']
     return (
       <div className="Schedule">
-        <div className='schedule-time'>
-          <div>Time</div>
-          {dailyTimeMilestone.map((time) => {
-            return (
-              <div key={time} className='time-milestone' >{time}</div>
-            )
-          })}
+        <div className='timer'>
+          <div className='schedule-time'>
+            <div>Time</div>
+            {dailyTimeMilestone.map((time) => {
+              return (
+                <div key={time} className='time-milestone' >{time}</div>
+              )
+            })}
+          </div>
+
+          <div className='schedule-event'>
+            <div>Event</div>
+            {dailyTimeMilestone.map((time) => {
+              return (
+                <div key={time} className='time-milestone' >{time}</div>
+              )
+            })}
+          </div>
         </div>
 
-        <div className='schedule-event'>
-          <div>Event</div>
-          {dailyTimeMilestone.map((time) => {
-            return (
-              <div key={time} className='time-milestone' >{time}</div>
-            )
-          })}
+        <div className='todo-detail'>
         </div>
       </div>
     )
