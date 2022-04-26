ab7fba93563e69ca613bd39e9a67431e0d965632
hungpv <phanhung2702@gmail.com>
2022-04-26 10:31:17 +0700

    [Add] option form-check scaffold

--- src/Options.js
+++ src/Options.js
@@ -5,6 +5,14 @@ export default class Option extends React.Component {
   render() {
     return (
       <div className="Option">
+        <div className='visibility'>
+          <div class="form-check">
+            <input class="form-check-input" type="checkbox" id='username' />
+            <label class="form-check-label" for="username">
+              This is first checkbox
+            </label>
+          </div>
+        </div>
       </div>
     )
   }
